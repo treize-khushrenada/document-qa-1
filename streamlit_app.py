abe4395a61eff313bce6c2bdf67ab756bcdfc3d4
arthur <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-03 00:05:23 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,54 @@
 import streamlit as st
 from openai import OpenAI
+import os
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 Medical Report - AI Translation")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Upload a document below and see it translated in bahasa malaysia "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# Create an OpenAI client.
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+    "Upload a document (.txt or .md)", type=("txt", "md")
+)
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
-    )
+# Ask the user for a question via `st.text_area`.
+# question = st.text_area(
+#     "Now ask a question about the document!",
+#     placeholder="Can you give me a short summary?",
+#     disabled=not uploaded_file,
+# )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+client = OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
 
-    if uploaded_file and question:
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
+if uploaded_file:
+
+    # Process the uploaded file.
+    document = uploaded_file.read().decode()
+
+    def get_answer_from_model(document, client):
         messages = [
             {
                 "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+                "content": f"Here's a document: {document} \n\n---\n\n translate it to Bahasa Malaysia. Return directly with the translated output in your answer",
             }
         ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
             messages=messages,
-            stream=True,
+            stream=False,
         )
+        return response.choices[0].message.content
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+    # Generate an answer using the OpenAI API.
+    answer = get_answer_from_model(document, client)
+
+    # Display the answer in the Streamlit app.
+    st.markdown(answer)
