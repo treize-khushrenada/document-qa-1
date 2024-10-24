3e474ea59004c485524e5183c92b0ae1488e5e75
arthur <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-24 22:35:24 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ from openai import AzureOpenAI
 import os
 # comment
 # Show title and description.
-if st.button("Start", use_container_width=True):
+if st.button("Start"):
     st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4", autoplay="true")
