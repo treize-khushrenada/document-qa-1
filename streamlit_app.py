82abe4365e0d354e26052408b951daea57b56c7f
arthur <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-24 22:30:48 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,4 @@ import os
 # comment
 # Show title and description.
 
-video_file = open("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4")
-video_bytes = video_file.read()
-
-st.video(video_bytes)
+st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4")
