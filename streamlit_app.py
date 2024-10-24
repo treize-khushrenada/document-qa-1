eed73a9790af15637b8ecaa46944f70e6cd3c48e
arthur <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-24 23:58:26 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import os
+import time
 # comment
 # Show title and description.
+
 if st.button("Start"):
-    st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4", autoplay=True)
+
+    col1, col2 = st.columns(2)
+    with col1:
+
+        st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4", autoplay=True)
+    
+    with col2:
+
+        for tick in range(10):
+            time.sleep(.5)
+            print("yah")
