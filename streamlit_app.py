7437c5bd30cafb1ffe07b7bc615533fd0bf7d156
arthur <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-24 22:37:26 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import os
 # comment
 # Show title and description.
 if st.button("Start"):
-    st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4", autoplay="true")
+    st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4", autoplay=True)
