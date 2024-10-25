6b94b674ce67ffbcc893312485757c6b6d524f43
arthur <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-25 01:06:31 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import os
 import time
-# comment
-# Show title and description.
+import pandas as pd
+
+import os
+import numpy as np
+
+df = pd.read_csv("./BreakingBadNews.csv")
+
 
 if st.button("Start"):
 
@@ -12,7 +16,11 @@ if st.button("Start"):
         st.video("https://socialanalyticsplus.net/demo/SKH/videos/Breakingbadnews.mp4", format="video/mp4", autoplay=True)
     
     with col2:
+        
+        series = df.iloc[0, :].to_frame().T
+        my_data_element = st.line_chart(series)
 
-        for tick in range(118):
+        for tick in range(117):
             time.sleep(1)
-            st.text("tick")
+            add_df = df.iloc[tick, :].to_frame().T
+            my_data_element.add_rows(add_df)
